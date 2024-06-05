d06e504c2b7c719d33135d19a866556faa5f2fd1
Ronnit Sagar <127775780+Ronnit44@users.noreply.github.com>
2024-06-05 11:27:14 +0530

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from .forms import  PostAdminForm
 from .models import Post,Category,Comment
 
 class CommentItemInLine(admin.TabularInline):
@@ -7,6 +7,7 @@ class CommentItemInLine(admin.TabularInline):
     raw_id_fields = ['post']
 
 class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
     search_fields = ['title','intro','body']
     list_display = ['title','slug','category','created_at','status']
     list_filter = ['category','created_at','status']
@@ -21,7 +22,7 @@ class CategoryAdmin(admin.ModelAdmin):
 class CommentAdmin(admin.ModelAdmin):
     list_display = ['name','post','created_at']
 
+admin.site.register(Post,PostAdmin)
 admin.site.register(Category,CategoryAdmin)
 admin.site.register(Comment,CommentAdmin)
 
-admin.site.register(Post,PostAdmin)
