e2d9666f839dfd5197e9fb0f941afbc2c01cd211
Ronnit Sagar <127775780+Ronnit44@users.noreply.github.com>
2024-06-05 11:28:41 +0530

    fixed no reverse error while submitting comment

--- blog/views.py
+++ blog/views.py
@@ -3,23 +3,37 @@ from django.db.models import Q
 from .models import Post,Category
 from .forms import CommentForm
 
-def detail(request,category_slug,slug):
-    post = get_object_or_404(Post,slug=slug,status=Post.ACTIVE)
+# def detail(request,category_slug,slug):
+#     post = get_object_or_404(Post,slug=slug,status=Post.ACTIVE)
 
-    if request.method =="POST":
+#     if request.method =="POST":
+#         form = CommentForm(request.POST)
+
+#         if form.is_valid():
+#             comment = form.save(commit=False)
+#             comment.post=post
+#             comment.save()
+
+#             return redirect('post_detail',category_slug = category_slug,slug=slug)
+
+#     else:
+#         form = CommentForm()
+
+#     return render(request,'blog/detail.html', {'post' : post,'form': form})
+def detail(request, category_slug, slug):
+    post = get_object_or_404(Post, slug=slug, status=Post.ACTIVE, category__slug=category_slug)
+
+    if request.method == "POST":
         form = CommentForm(request.POST)
-
         if form.is_valid():
             comment = form.save(commit=False)
-            comment.post=post
+            comment.post = post
             comment.save()
-
-            return redirect('post_detail',category_slug = category_slug,slug=slug)
-
+            return redirect('post_detail', category_slug=category_slug, slug=slug)
     else:
         form = CommentForm()
 
-    return render(request,'blog/detail.html', {'post' : post,'form': form})
+    return render(request, 'blog/detail.html', {'post': post, 'form': form})
 
 def category(request,slug):
     category = get_object_or_404(Category,slug=slug)
