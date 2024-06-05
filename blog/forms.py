fa7b3c689a55cad6a63cfd4d25dbd6c2f5cc567b
Ronnit Sagar <127775780+Ronnit44@users.noreply.github.com>
2024-06-05 11:25:55 +0530

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 from .models import Comment
+from django.utils import timezone
 
 class CommentForm(forms.ModelForm):
     class Meta():
         model = Comment
         fields = ('name','email','body')
+
+class PostAdminForm(forms.ModelForm):
+    created_at = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}), input_formats=['%Y-%m-%dT%H:%M'])
+
+    class Meta:
+        model = Post
+        fields = '__all__'
