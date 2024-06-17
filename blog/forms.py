ef7b5a346fb6363846b471cec0c65c7e0c4963ab
Ronnit Sagar <127775780+Ronnit44@users.noreply.github.com>
2024-06-18 01:06:13 +0530

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -6,17 +6,14 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('name','email','body')
-<<<<<<< HEAD
-=======
 
->>>>>>> 5a4eed5039ef24126696f1c0d815e0ccfe89f614
 class PostAdminForm(forms.ModelForm):
     created_at = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}), input_formats=['%Y-%m-%dT%H:%M'])
 
     class Meta:
         model = Post
-<<<<<<< HEAD
+
         fields = '__all__'
-=======
+
         fields = '__all__'
->>>>>>> 5a4eed5039ef24126696f1c0d815e0ccfe89f614
+
